884f4e50e2aba88c677b3e7593a73839112b10da
Maxim Sivakon <maximsivakon@Maxims-MBP.tknsib.local>
2024-05-06 14:29:03 +0700

    save work

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,34 +1,56 @@
 window.addEventListener('scroll', trackscroll);
 
 function trackscroll() {
-    let header = document.getElementById('header');
-    if (window.pageYOffset > 0) {
-        header.classList.add('-top-11');
-    } else {
-        header.classList.remove('-top-11');
+    if (window.innerWidth >= 1024) {
+        let header = document.getElementById('header');
+        if (window.pageYOffset > 0) {
+            header.classList.add('-top-11');
+        } else {
+            header.classList.remove('-top-11');
+        }
     }
 }
 
 // search line
-function initSearch() {
-    var controlSearchPc = document.getElementById("control-search-pc");
-    var btnSearchActionClose = document.getElementById("btn-search-action-close");
-    var mainSearch = document.getElementById("main-search");
-    var mainSearchBlur = document.querySelector('#main-search > .body-blur');
-
-    controlSearchPc.onclick = function () {
-        mainSearch.style.display = 'block';
-    }
-    btnSearchActionClose.onclick = function () {
-        mainSearch.style.display = 'none';
-    }
-    mainSearchBlur.onclick = function () {
-        mainSearch.style.display = 'none';
-    }
-}
-
 document.addEventListener('readystatechange', function () {
     if (document.readyState === "complete") {
-        initSearch();
+        var controlSearchMb = document.getElementById("control-search-mb");
+        var controlSearchPc = document.getElementById("control-search-pc");
+        var btnSearchActionClose = document.getElementById("btn-search-action-close");
+        var mainSearch = document.getElementById("main-search");
+        var mainSearchBlur = document.querySelector('#main-search > .body-blur');
+
+        controlSearchMb.onclick = function () {
+            mainSearch.style.display = 'block';
+        }
+        controlSearchPc.onclick = function () {
+            mainSearch.style.display = 'block';
+        }
+        btnSearchActionClose.onclick = function () {
+            mainSearch.style.display = 'none';
+        }
+        mainSearchBlur.onclick = function () {
+            mainSearch.style.display = 'none';
+        }
+    }
+});
+
+// mobile menu
+document.addEventListener('readystatechange', function () {
+    if (document.readyState === "complete") {
+        var mainMobileMenuOpen = document.getElementById("main-mobile-menu-open");
+        var mobileMenuClose = document.getElementById("mobile-menu-close");
+        var mainMobileMenuBar = document.getElementById("main-mobile-menu-bar");
+        var mainMobileMenuBarBlur = document.getElementById("main-mobile-menu-bar-blur");
+
+        mainMobileMenuOpen.onclick = function () {
+            mainMobileMenuBar.style.display = 'block';
+        }
+        mobileMenuClose.onclick = function () {
+            mainMobileMenuBar.style.display = 'none';
+        }
+        mainMobileMenuBarBlur.onclick = function () {
+            mainMobileMenuBar.style.display = 'none';
+        }
     }
 });
--- src/assets/js/modals.js
+++ src/assets/js/modals.js
@@ -1,10 +1,14 @@
 // modal location
 function initLocation() {
+    var selectGeoLocationMb = document.getElementById("select-geo-location-mb");
     var selectGeoLocationPc = document.getElementById("select-geo-location-pc");
     var modalLocationCloseBtn = document.getElementById("modal-location-close-btn");
     var mainLocation = document.getElementById("main-location");
     var mainLocationBlur = document.querySelector('#main-location > .body-blur');
 
+    selectGeoLocationMb.onclick = function () {
+        mainLocation.style.display = 'block';
+    }
     selectGeoLocationPc.onclick = function () {
         mainLocation.style.display = 'block';
     }
