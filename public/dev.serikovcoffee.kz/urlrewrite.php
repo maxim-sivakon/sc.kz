5985412087d56158774e7add0f4e99c94a00084f
Production Server <dev@serikovcoffee.kz>
2024-09-08 15:01:00 +0300

    save blog and fix urlrewrite

--- public/dev.serikovcoffee.kz/urlrewrite.php
+++ public/dev.serikovcoffee.kz/urlrewrite.php
@@ -1,19 +1,27 @@
 <?php
-$arUrlRewrite=array (
-  2 => 
-  array (
-    'CONDITION' => '#^/catalog/#',
-    'RULE' => '',
-    'ID' => 'bitrix:catalog',
-    'PATH' => '/catalog/index.php',
-    'SORT' => 100,
-  ),
-  1 => 
-  array (
-    'CONDITION' => '#^/rest/#',
-    'RULE' => '',
-    'ID' => NULL,
-    'PATH' => '/bitrix/services/rest/index.php',
-    'SORT' => 100,
-  ),
-);
+$arUrlRewrite = [
+    2 =>
+        [
+            "CONDITION" => "#^/catalog/#",
+            'RULE' => '',
+            'ID'        => 'bitrix:catalog',
+            'PATH'      => '/catalog/index.php',
+            'SORT'      => 100,
+        ],
+    1 =>
+        [
+            'CONDITION' => '#^/rest/#',
+            'RULE'      => '',
+            'ID'        => null,
+            'PATH'      => '/bitrix/services/rest/index.php',
+            'SORT'      => 100,
+        ],
+    3 =>
+        [
+            "CONDITION" => "#^/blog/#",
+            'RULE' => '',
+            'ID'        => 'bitrix:catalog',
+            'PATH'      => '/blog/index.php',
+            'SORT'      => 100,
+        ],
+];
